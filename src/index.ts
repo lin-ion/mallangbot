7fd455cb8c65119ed01fe4bc7e21373852fdcf7e
lin-ion <lin-ion@naver.com>
2025-05-03 04:27:02 +0000

    feat: add slash command and event registration logic

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import dotenvFlow from 'dotenv-flow';
+import fs from 'fs';
+import path from 'path';
+import { CustomClient, DiscordCommand, DiscordEvent } from './types/discord.js';
 dotenvFlow.config();
 
 const client = new Client({
     intents: [GatewayIntentBits.GuildMessages],
-});
+}) as CustomClient;
 
-client.on('ready', (client) => {
-    const name = process.env.CLIENT_NAME;
-    if (name) {
-        client.user?.setUsername(name);
-    }
-    console.log(`Logged in as ${client.user?.tag}`);
-});
+client.commands = new Map();
 
-const token = process.env.TOKEN;
-if (token) {
-    client.login(token);
+const commandsPath = path.join(import.meta.dirname, 'commands/slash');
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.ts') || file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = (await import(filePath)).default as DiscordCommand;
+    client.commands.set(command.data.name, command);
 }
 
-process.on('SIGTERM', () => {
-    process.exit(0);
-});
+const eventsPath = path.join(import.meta.dirname, 'events');
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith('.ts') || file.endsWith('.js'));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = (await import(filePath)).default as DiscordEvent;
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(process.env.TOKEN);

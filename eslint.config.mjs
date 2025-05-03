91c712e542ffcd1990f5df0ab415931a4426292b
lin-ion <lin-ion@naver.com>
2025-05-03 00:53:59 +0000

    Update linting and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,6 @@ import prettier from 'eslint-plugin-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-    files: ['**/*.ts'],
     plugins: { prettier },
     rules: {
         'prettier/prettier': 'error',

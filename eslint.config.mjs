254d372d30e6dbf081a01b0a94326f0117d3fb56
lin-ion <lin-ion@naver.com>
2025-05-04 03:42:25 +0000

    chore: update dependencies and configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,26 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-    plugins: { prettier },
-    rules: {
-        'prettier/prettier': 'error',
+export default tseslint.config(
+    {
+        ignores: [
+            '**/dist/**'
+        ]
     },
-});
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    prettierConfig,
+    {
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                tsconfigRootDir: import.meta.dirname,
+                ecmaVersion: 'latest',
+            },
+            sourceType: 'module',
+        },
+    }
+);
